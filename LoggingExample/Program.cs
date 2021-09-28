499156ace4c18063d2855cce89ef22205ea1b69d
DoraBabu <mrdorababu@gmail.com>
2021-09-28 15:22:50 +0530

    Init Commit

---
+++ LoggingExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace LoggingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\path\log.txt";
+            var displayMessage = "Hello, user";
+            File.WriteAllText(path, displayMessage);
+            Console.WriteLine(displayMessage);
+        }
+    }
+}
+
+
